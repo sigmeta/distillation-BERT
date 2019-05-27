ee8a57f9b6e96030b2523dc6d4dbf56b3cf845a8
Sigmeta <sigmeta@pku.edu.cn>
2019-05-27 12:30:43 +0800

    sentiment classification

--- examples/run_sentiment.py
+++ examples/run_sentiment.py
@@ -96,7 +96,7 @@ class SentProcessor(object):
 
     def get_labels(self):
         """See base class."""
-        return ["0","1"]
+        return [0,1]
 
     def _create_examples(self, lines, set_type):
         """Creates examples for the training and dev sets."""
