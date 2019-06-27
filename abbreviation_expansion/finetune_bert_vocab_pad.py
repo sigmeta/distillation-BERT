f3de5fda55af4ee9ebd5f2217050be8c204b27f8
Sigmeta <sigmeta@pku.edu.cn>
2019-06-27 16:11:25 +0800

    bug fixing

--- abbreviation_expansion/finetune_bert_vocab_pad.py
+++ abbreviation_expansion/finetune_bert_vocab_pad.py
@@ -242,7 +242,7 @@ def convert_examples_to_features(examples, label_list, max_seq_length, tokenizer
         # assert tokens[label_pos]==example.label[-1][1][0]
         candidates=[exab[example.label]]+list(abex[exab[example.label]].keys())
 
-        char=label.split('\t')[0]
+        char=''
 
 
         if ex_index < 5:
